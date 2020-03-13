535dda2e029ef7187ec777bf7fc8c9c6cd29ca84
Jthom9 <58039328+Jthom9@users.noreply.github.com>
2020-03-13 10:59:13 +0100

    Add files via upload

--- Autenticazione.java
+++ Autenticazione.java
@@ -1,29 +1,69 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Autenticazione {
-	public String username;
-	public String password;
+	private String username;
+	private String password;
+	private String file;
 	
-	public Autenticazione(String password, String username)
-	{
-		this.password = password;
-		this.username = username;
+	private static Scanner x;	
+
+	public Autenticazione(String username, String password){
+	    this.username = username;
+	    this.password = password;
 	}
+
 	public Autenticazione()
 	{
-		username = "";
 		password = "";
+		username = "";
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	public static void verificaLogin(String username, String password, String file)
+ 	{
+		boolean cerca = false;
+		String tempUsername = "";
+		String tempPassword = "";
+		
+		try
+		{
+			x = new Scanner(new File(file));
+			x.useDelimiter("[,\n]");
+			
+			while(x.hasNext() && !cerca)
+			{
+				tempUsername = x.next();
+				tempPassword = x.next();
+				
+				if(tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim()))
+				{
+					cerca = true;
+				}
+			}
+			x.close();
+			System.out.println(cerca);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Errore");
+		}
+ 	}
+
 
 }
