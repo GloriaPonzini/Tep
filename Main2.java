00580a9bb3762d416bbe85a2c4e535504ffba2e8
GloriaPonzini <gponzini@studenti.iissgadda.it>
2020-04-01 08:33:53 +0200

    Realizzazione main

---
+++ Main2.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		//MENU'
+		
+		System.out.println("Cosa vuoi fare?");
+		
+		
+		System.out.println("0-Uscire");
+		System.out.println("1-Creare zona e fermata");
+		
+		
+		//acquisisco il valore per la scelta
+		
+		Scanner tastiera= new Scanner (System.in);
+		int scelta=tastiera.nextInt();
+		
+		//Effettuo la verifica per l'evento selezionato
+		
+		if( scelta==0) {
+			System.out.println("Fine");
+		}
+		
+				
+	
+	if(scelta==1) {
+		
+		System.out.println("Inserisci il numero della zona");
+		int zona=tastiera.nextInt();
+		tastiera.nextLine();
+		System.out.println("Inserisci il nome della fermata");
+		String fermata=tastiera.nextLine();
+		Fermata f = new Fermata(zona, fermata);
+		f.addFermata();
+	}
+	
+
+
+	
+	if(scelta !=0 && scelta!=1 ){
+		System.out.println("Il valore inserito non è corretto, riprova");
+	}
+}
+}
