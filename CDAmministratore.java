57d2cfc2de74a62c6398604c2f07d9da5a52313a
Chezia <60304832+Chezia@users.noreply.github.com>
2020-04-06 13:59:41 +0200

    Creazine classe Amministratore

---
+++ CDAmministratore.java
@@ -0,0 +1,70 @@
+
+package jgvf;
+
+public class Amministratore {
+	private String username;
+	private String password;
+	private String nome;
+	private String cognome;
+	private String email;
+	
+	public Amministratore(String username,String password,String nome,String cognome,String email) {
+		this.username = username;
+		this.password = password;
+		this.cognome = cognome;
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public Amministratore() {
+		password = "";
+		nome = "";
+		username = "";
+		email = "";
+		cognome = "";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return "Username =" + username + ", Password =" + password + ", nome =" + nome + ", Cognome =" + cognome + ",e-maill =" + email;
+	}
+}
